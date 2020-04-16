6ae1f13ffef017e6817d603787804d67ddba9966
dustypaulson <dustyplsn@gmail.com>
2020-04-16 12:48:59 -0500

    Update windows-and-linux-diagnostic-extension-with-table-and-container-sink-as-job.ps1

--- runbooks/Diagnostic Extensions/windows-and-linux-diagnostic-extension-with-table-and-container-sink-as-job.ps1
+++ runbooks/Diagnostic Extensions/windows-and-linux-diagnostic-extension-with-table-and-container-sink-as-job.ps1
@@ -346,7 +346,7 @@ foreach ($vm in $vms) {
 		Remove-Variable windowsExtensionCheck -Force -Confirm:$false
 		Remove-Variable status -Force -Confirm:$false
 		Remove-Variable vm -Force -Confirm:$false
-		Remove-Variable sa -Force -Confirm:$false
+		if ($sa -ne $null) {Remove-Variable sa -Force -Confirm:$false}
 		if ($sasToken -ne $null) { Remove-Variable sasToken -Force -Confirm:$false }
 		[System.GC]::GetTotalMemory($true) | Out-Null
 		Start-Sleep -s 10
@@ -1085,7 +1085,7 @@ foreach ($vm in $vms) {
 		Remove-Variable windowsExtensionCheck -Force -Confirm:$false
 		Remove-Variable status -Force -Confirm:$false
 		Remove-Variable vm -Force -Confirm:$false
-		Remove-Variable sa -Force -Confirm:$false
+		if ($sa -ne $null) {Remove-Variable sa -Force -Confirm:$false}
 		if ($sasToken -ne $null) { Remove-Variable sasToken -Force -Confirm:$false }
 		[System.GC]::GetTotalMemory($true) | Out-Null
 		Start-Sleep -s 10
