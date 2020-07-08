5d86e45c362e8cf7689d6a3602a18408c4598661
dustypaulson <dustyplsn@gmail.com>
2020-07-08 10:49:02 -0500

    Create Memory - Redhat FTK CLI.sh

---
+++ PowerShell Scripts/Forensics/On Prem/Memory - Redhat FTK CLI.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+forensics=$(blkid -L FORENSICS)
+os=$(blkid -L cloudimg-rootfs)
+if pgrep -l -x ftkimager > /dev/null && find /mnt/forensics -name "$(hostname -s)".E01.txt > /dev/null
+then
+while pgrep -l -x ftkimager > /dev/null && find /mnt/forensics -name "$(hostname -s)".E01.txt > /dev/null;do
+echo "FTK is running and text file has not been created."
+sleep 30s
+done
+cd /
+umount "$forensics" /mnt/forensics
+rm -r /mnt/forensics
+else
+mkdir /mnt/forensics
+mount -L FORENSICS /mnt/forensics
+cd /mnt/forensics || exit
+mkdir "$(hostname -s)"
+/mnt/forensics/ftkimager "$os" /mnt/forensics/"$(hostname -s)"/"$(hostname -s)" --e01 --frag 2G --compress 9 --verify
+cd /
+umount "$forensics" /mnt/forensics
+rm -r /mnt/forensics
+fi
