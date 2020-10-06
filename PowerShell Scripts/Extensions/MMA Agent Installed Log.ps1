1ef54fcd26f869de8db60f9e07b1183b4c43021a
dustypaulson <dustyplsn@gmail.com>
2020-10-06 14:51:06 -0500

    Add files via upload

---
+++ PowerShell Scripts/Extensions/MMA Agent Installed Log.ps1
@@ -0,0 +1,25 @@
+﻿$RGs = Get-AzResourceGroup
+foreach ($rg in $RGs) {
+	$VMs = Get-AzVM -ResourceGroupName $rg.ResourceGroupName
+	foreach ($vm in $VMs) {
+		if ($vm.StorageProfile.OsDisk.OsType -eq "Linux") {
+			$ext = Get-AzVMExtension -ResourceGroupName $rg.ResourceGroupName -VMName $vm.Name | Where-Object { $_.ExtensionType -eq "MicrosoftMonitoringAgent" }
+			if ($ext -eq $null) {
+				$text = "install on linux vm $($vm.Name) in RG $($vm.ResourceGroupName)" | Out-File "C:\Users\dusty\Desktop\Extension Installed.txt" -Append ascii -NoClobber -Force
+			}
+			if ($ext -ne $null) {
+				$text = "extension exists on linux vm $($vm.Name) in RG $($vm.ResourceGroupName)" | Out-File "C:\Users\dusty\Desktop\Extension Installed.txt" -Append ascii -NoClobber -Force
+			}
+
+		}
+		if ($vm.StorageProfile.OsDisk.OsType -eq "Windows") {
+			$ext = Get-AzVMExtension -ResourceGroupName $rg.ResourceGroupName -VMName $vm.Name | Where-Object { $_.ExtensionType -eq "MicrosoftMonitoringAgent" }
+			if ($ext -eq $null) {
+				$text = "install on windows vm $($vm.Name) in RG $($vm.ResourceGroupName)" | Out-File "C:\Users\dusty\Desktop\Extension Installed.txt" -Append ascii -NoClobber -Force
+			}
+			if ($ext -ne $null) {
+				$text = "extension exists on windows vm $($vm.Name) in RG $($vm.ResourceGroupName)" | Out-File "C:\Users\dusty\Desktop\Extension Installed.txt" -Append ascii -NoClobber -Force
+			}
+		}
+	}
+}
---
+++ PowerShell Scripts/Extensions/Remove Custom Script Extension.ps1
@@ -0,0 +1,2 @@
+﻿#Remove custom script extension
+Remove-AzVMCustomScriptExtension -ResourceGroupName password_spray_demo -VMName $attackVM -Name DownloadPasswordSprayFiles -Force -Confirm:$false
