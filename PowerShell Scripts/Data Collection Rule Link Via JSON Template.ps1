b72cd36e5443d0ca4e6503c12dbd6ca211823e36
dustypaulson <dustyplsn@gmail.com>
2022-08-19 13:45:57 -0500

    Add files via upload

---
+++ PowerShell Scripts/Data Collection Rule Link Via JSON Template.ps1
@@ -0,0 +1,14 @@
+﻿param(
+$defaultDataCollectionRuleResourceId,
+$workspaceID
+)
+
+$defaultDcrParams = @"
+{
+    "properties": {
+        "defaultDataCollectionRuleResourceId": "$defaultDataCollectionRuleResourceId"
+    }
+}
+"@
+$workspaceString = "$workspaceID" + "?api-version=2021-12-01-preview"
+Invoke-AzRestMethod -Path "$workspaceString"  -Method PATCH -payload $defaultDcrParams
